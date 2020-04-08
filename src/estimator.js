3d145d29d272b09ea211991fc573ce2b4ad6c181
siccoo <obichilaka@gmail.com>
2020-04-08 14:54:33 +0100

    started evaluation

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const data = input;
+    return {
+        data: input,
+        impact: {},
+        severeImpact: {}
+    }
+};
 
 export default covid19ImpactEstimator;
