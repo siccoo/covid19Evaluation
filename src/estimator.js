4f06233a1b206c098640edf1822f54c90d09583e
siccoo <obichilaka@gmail.com>
2020-04-09 09:54:47 +0100

    Attempting challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,30 @@
+const covid19ImpactEstimator = (reportedCases) => {
+    const currentlyInfected = reportedCases * 10;
+    return currentlyInfected = impact.currentlyInfected; 
+};
+
+
 const covid19ImpactEstimator = (data) => {
-    const data = input;
-    return {
-        data: input,
+    const estimator = chain(
+        // CHALLENGE ONE
+        estimateCurrentlyInfected,
+        estimateProjectedInfections,
+
+        // CHALLENGE TWO
+        estimateSevereCases,
+        estimateBedSpaceAvailability,
+
+        // CHALLENGE THREE
+        estimateCasesForICU,
+        estimateCasesForVentilators,
+        estimateDollarsInFlight
+    );
+
+    return estimator ({
+        data,
         impact: {},
         severeImpact: {}
-    }
+    });
 };
 
 export default covid19ImpactEstimator;
